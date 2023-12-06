fbdb609051f6c325c75962f0bc9b516702892c7b
josuhinrichs <josue2001marinho@gmail.com>
2023-12-06 23:23:49 +0100

    Adicionando integração com API

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, jsonify
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from flask import Flask, render_template, request
+from transformers import AutoTokenizer
 import nltk
-from sklearn.feature_extraction.text import TfidfVectorizer
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from joblib import load
+import json
+import requests
 
 
 nltk.download('wordnet')
@@ -13,6 +12,7 @@ nltk.download('stopwords')
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 #model = load('trained_model_SVM.joblib')
 
+url = 'https://mandacaru-sentiment-api.onrender.com/analise-sentimento'
 
 app = Flask(__name__)
 
@@ -23,8 +23,22 @@ def index():
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    return get_Chat_response(input)
+    input = get_Chat_response(msg)
+
+    data = {
+    'texto': input
+    }
+
+    json_data = json.dumps(data)
+    
+    response = requests.post(url, headers={'Content-Type': 'application/json'}, data=json_data)
+        # Parse the JSON into a Python dictionary
+    response_dict = json.loads(response.text)
+
+    # Extract the first element of the list associated with the 'sentimento' key
+    sentimento = response_dict["sentimento"][0]
+    
+    return sentimento.capitalize()
 
 def get_Chat_response(text):
 
@@ -40,6 +54,7 @@ def get_Chat_response(text):
         # Decode the output using the tokenizer (assuming it's a Hugging Face Transformers tokenizer)
         response = tokenizer.decode(output[0], skip_special_tokens=True)
         return response
+    
 def normalize(text):
     tokens = nltk.word_tokenize(text)
     stopwords = nltk.corpus.stopwords.words('english')
